8107a8ce9c5b11169e79058a2de1c5bc568a3f05
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-04-25 15:48:51 -0700

    Updated

--- main.h
+++ main.h
@@ -4,16 +4,25 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stddef.h>
+#include <stddef.h>
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int p_char(va_list ch);
 int p_string(va_list st);
 
+
+/**
+ * struct myformat - this is the struct
+ * @type: is a type
+ * @func: is a pointer to func
+ * Description: this is myformat struct
+*/
+
 typedef struct myformat
 {
-    char *type;
-    int (*func)(va_list);
-}print_type;
+	char *type;
+	int (*func)(va_list);
+} print_type;
 
 #endif
