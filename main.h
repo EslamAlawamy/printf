212e7cd6b2bdafaaec392e8a5fc56709aac314c4
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-04-25 17:48:04 -0700

    update

--- main.h
+++ main.h
@@ -5,8 +5,8 @@
 #include <unistd.h>
 #include <stdio.h>
 
+int _putchar(char c);
 int _printf(const char *format, ...);
-int print_char(char character);
 int print_string(char *string);
 void int_to_string(int number, char *buffer);
 
