4de8bb55f2d5fe1bf70633af4ea930e838fe4184
ahmedmorgan210 <ahmedmorgan210@gmail.com>
2023-04-25 17:41:40 -0700

    update

---
+++ printf.c
@@ -0,0 +1,64 @@
+#include "main.h"
+/**
+ * print_string - the main func
+ * @string: th var
+ * Return: success
+ */
+int print_string(char *string)
+{
+	int i = 0;
+
+	if (string == NULL)
+	{
+		string = "(null)";
+	}
+	while (string[i] != '\0')
+	{
+		_putchar(string[i]);
+		i++;
+	}
+	return (i);
+}
+/**
+ * _printf - my main func
+ * @format: the format in the func
+ * Return: success
+ */
+int _printf(const char *format, ...)
+{
+	va_list fptr;
+	int tlen = 0;
+	int i = 0;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+	va_start(fptr, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'c':
+					tlen = tlen + _putchar(va_arg(fptr, int));
+					break;
+				case 's':
+					tlen = tlen + print_string(va_arg(fptr, char *));
+					break;
+				case '%':
+					tlen = tlen + _putchar('%');
+					break;
+				default:
+					return (-1);
+			}
+		}
+		else
+			tlen = tlen + _putchar(format[i]);
+			i++;
+	}
+	va_end(fptr);
+	return (tlen);
+}
